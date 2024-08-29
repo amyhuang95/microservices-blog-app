3cdd14d4371502e97d9b76601f4db4217531980c
amyhuang95 <62624198+amyhuang95@users.noreply.github.com>
2024-08-29 10:43:06 -0700

    replaced localhost to ClusterIP

--- comments/index.js
+++ comments/index.js
@@ -43,7 +43,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   commentsByPostId[req.params.id] = comments;
 
   // Emit event to event bus
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "CommentCreated",
     data: {
       id: commentId,
@@ -74,7 +74,7 @@ app.post("/events", async (req, res) => {
     comment.status = status;
 
     // Emit event to event bus
-    await axios.post("http://localhost:4005/events", {
+    await axios.post("http://event-bus-srv:4005/events", {
       type: "CommentUpdated",
       data: {
         id,
